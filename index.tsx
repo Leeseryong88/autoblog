1a5e086a1eeec4fc13b6a167bd90b3b19766219b
Leeseryong88 <leeseryong88@gmail.com>
2026-02-02 17:49:07 +0900

    Add Vercel Analytics

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -16,6 +17,7 @@ root.render(
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <Analytics />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
